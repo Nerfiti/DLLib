950e827632d45f6c389513ce515a89ee82514cf3
vFlameCat <alexraketen.3@gmail.com>
2026-08-15 01:21:26 +0300

    done

---
+++ app/main.cpp
@@ -0,0 +1,18 @@
+#include <CDLib.h>
+
+
+int main ()
+{
+    createWindow(550, 800, "CDLib");
+
+    setColor(sf::Color::Magenta);
+    setFillColor(sf::Color::Transparent);
+    setThinkness(3.f);
+
+    drawCircle(150, 650, 100);
+    drawCircle(400, 650, 100);
+
+    drawEllipse(175, 50, 375, 600);
+
+    return 0;
+}
