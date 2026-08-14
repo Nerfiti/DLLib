eb46053f8bcfe74c73cb0571c17fe34430645850
Nerfiti <112069471+Nerfiti@users.noreply.github.com>
2026-08-14 22:31:04 +0300

    Move color and thinkness to global state

--- DLLib.h
+++ DLLib.h
@@ -85,13 +85,21 @@ namespace commands
 {
     std::thread _user_thread;
 
+    struct DrawState final
+    {
+        sf::Color fillColor = sf::Color::Transparent;
+        sf::Color outlineColor = sf::Color::Transparent;
+        
+        float thinkness;
+    } _draw_state;
+
+    using cmd_t = std::function<void(DrawState&)>;
+    std::vector<cmd_t> _commands;
+
     //TODO: lock-free commands
     std::atomic<bool> _command_list_full = false;
     std::mutex _command_list_mutex;
     std::condition_variable _command_list_ready_to_write;
-
-    using cmd_t = std::function<void()>;
-    std::vector<cmd_t> _commands;
 
     void addCommand(cmd_t&& command)
     {
@@ -107,7 +115,7 @@ namespace commands
         _command_list_full.store(true);
     }
 
-    void executeCommands()
+    void executeCommands(DrawState& draw_state)
     {
         if (_command_list_full.load() == false)
             return;
@@ -124,10 +132,11 @@ namespace commands
 
         for (auto& command : cmds)
         {
-            command();
+            command(draw_state);
         }
     }
 };
+
 };
 
 //---------------------------------------------------------
@@ -136,7 +145,7 @@ using detail::windows::window_handler_t;
 
 void display()
 {
-    detail::commands::addCommand([] 
+    detail::commands::addCommand([] (detail::commands::DrawState&)
     {
         detail::windows::displayWindows();
     });
@@ -147,7 +156,8 @@ window_handler_t createWindow(uint32_t width, uint32_t height, std::string_view 
 {
     auto promise = std::make_shared<std::promise<window_handler_t>>();
     auto future = promise->get_future();
-    detail::commands::addCommand([=] () mutable
+
+    detail::commands::addCommand([=] (detail::commands::DrawState&) mutable
     {
         window_handler_t wh = detail::windows::createWindow(width, height, title, state);
         detail::windows::displayWindow(*wh);
@@ -159,16 +169,41 @@ window_handler_t createWindow(uint32_t width, uint32_t height, std::string_view 
     return future.get();
 }
 
-void clear(sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+//---------------------------------------------------------
+
+void setFillColor(sf::Color color)
 {
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
     {
-        window_handler->texture.clear(color);
+        state.fillColor = color;
     });
 }
 
-//TODO: move color and thinkness to the global state
-void drawLine(int x0, int y0, int x1, int y1, sf::Color color, float thinkness, window_handler_t window_handler = detail::windows::getLastWindow())
+void setOutlineColor(sf::Color color)
+{
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
+    {
+        state.outlineColor = color;
+    });
+}
+
+void setThinkness(float thinkness)
+{
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
+    {
+        state.thinkness = thinkness;
+    });
+}
+
+void clear(window_handler_t window_handler = detail::windows::getLastWindow())
+{
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
+    {
+        window_handler->texture.clear(state.fillColor);
+    });
+}
+
+void drawLine(int x0, int y0, int x1, int y1, window_handler_t window_handler = detail::windows::getLastWindow())
 {
     if (x1 < x0)
     {
@@ -180,24 +215,27 @@ void drawLine(int x0, int y0, int x1, int y1, sf::Color color, float thinkness, 
     int64_t dy = y1 - y0;
     float len = sqrt(dx * dx + dy * dy);
 
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
     {
-        sf::RectangleShape line(sf::Vector2f(len, thinkness));
-        line.setPosition(sf::Vector2f(x0, y0));
-        line.setFillColor(color);
+        sf::RectangleShape line(sf::Vector2f(len, state.thinkness));
+        line.setFillColor(state.fillColor);
 
+        line.setPosition(sf::Vector2f(x0, y0));
         line.rotate(sf::radians(std::atan(static_cast<float>(dy)/dx)));
 
         window_handler->texture.draw(line);
     });
 }
 
-void drawCircle(int center_x, int center_y, float radius, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void drawCircle(int center_x, int center_y, float radius, window_handler_t window_handler = detail::windows::getLastWindow())
 {
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
     {
         sf::CircleShape circle(radius);
-        circle.setFillColor(color);
+        circle.setFillColor(state.fillColor);
+        circle.setOutlineColor(state.outlineColor);
+        circle.setOutlineThickness(state.thinkness);
+
         circle.setOrigin(sf::Vector2f(radius, radius));
         circle.setPosition(sf::Vector2f(center_x, center_y));
 
@@ -205,7 +243,7 @@ void drawCircle(int center_x, int center_y, float radius, sf::Color color, windo
     });
 }
 
-void drawEllipse(int x0, int y0, int x1, int y1, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void drawEllipse(int x0, int y0, int x1, int y1, window_handler_t window_handler = detail::windows::getLastWindow())
 {
     if (x1 < x0)
     {
@@ -216,10 +254,13 @@ void drawEllipse(int x0, int y0, int x1, int y1, sf::Color color, window_handler
     int64_t width = x1 - x0 + 1;
     int64_t height = y1 - y0 + 1;
 
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
     {
         sf::CircleShape ellipse(height);
-        ellipse.setFillColor(color);
+        ellipse.setFillColor(state.fillColor);
+        ellipse.setOutlineColor(state.outlineColor);
+        ellipse.setOutlineThickness(state.thinkness);
+
         ellipse.scale(sf::Vector2f(static_cast<float>(width) / height, 1.0f));
         ellipse.setPosition(sf::Vector2f(x0, y0));
 
@@ -227,7 +268,7 @@ void drawEllipse(int x0, int y0, int x1, int y1, sf::Color color, window_handler
     });
 }
 
-void drawRect(int x0, int y0, int x1, int y1, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void drawRect(int x0, int y0, int x1, int y1, window_handler_t window_handler = detail::windows::getLastWindow())
 {
     if (x1 < x0)
     {
@@ -238,42 +279,47 @@ void drawRect(int x0, int y0, int x1, int y1, sf::Color color, window_handler_t 
     int64_t width = x1 - x0 + 1;
     int64_t height = y1 - y0 + 1;
 
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state)
     {
         sf::RectangleShape rect(sf::Vector2f(width, height));
+        rect.setFillColor(state.fillColor);
+        rect.setOutlineColor(state.outlineColor);
+        rect.setOutlineThickness(state.thinkness);
+
         rect.setPosition(sf::Vector2f(x0, y0));
-        rect.setFillColor(color);
 
         window_handler->texture.draw(rect);
     });
 }
 
-void drawPolygon(sf::Vector2f points[], uint32_t num_points, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void drawPolygon(sf::Vector2f points[], uint32_t num_points, window_handler_t window_handler = detail::windows::getLastWindow())
 {
     sf::ConvexShape polygon(num_points);
-    polygon.setFillColor(color);
-
     for (size_t i = 0; i < num_points; ++i)
         polygon.setPoint(i, points[i]);
 
-    detail::commands::addCommand([=]
+    detail::commands::addCommand([=] (detail::commands::DrawState& state) mutable
     {
+        polygon.setFillColor(state.fillColor);
+        polygon.setOutlineColor(state.outlineColor);
+        polygon.setOutlineThickness(state.thinkness);
+
         window_handler->texture.draw(polygon);
     });
 }
 
-void drawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void drawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, window_handler_t window_handler = detail::windows::getLastWindow())
 {
     constexpr size_t kNumPoints = 3;
     sf::Vector2f points[kNumPoints] = {sf::Vector2f(x0, y0), sf::Vector2f(x1, y1), sf::Vector2f(x2, y2)};
 
-    drawPolygon(points, kNumPoints, color, window_handler);
+    drawPolygon(points, kNumPoints, window_handler);
 }
 
 //TODO: Ensure that float conversion does not break the operation
-void setPixel(int x, int y, sf::Color color, window_handler_t window_handler = detail::windows::getLastWindow())
+void setPixel(int x, int y, window_handler_t window_handler = detail::windows::getLastWindow())
 {
-    drawRect(x, y, x, y, color, window_handler);
+    drawRect(x, y, x, y, window_handler);
 }
 
 sf::Color getPixel(int x, int y, window_handler_t window_handler = detail::windows::getLastWindow())
@@ -281,7 +327,7 @@ sf::Color getPixel(int x, int y, window_handler_t window_handler = detail::windo
     auto promise = std::make_shared<std::promise<sf::Color>>();
     auto future = promise->get_future();
 
-    detail::commands::addCommand([=] () mutable
+    detail::commands::addCommand([=] (detail::commands::DrawState&) mutable
     {
         sf::Color pixel = window_handler->texture.getTexture().copyToImage().getPixel(sf::Vector2u(x, y));
         promise->set_value(pixel);
@@ -309,6 +355,7 @@ int main()
         user_thread_finished.store(true);
     });
 
+    detail::commands::DrawState draw_state = {};
     while(true)
     {
         if (user_thread_finished && detail::windows::_window_contexts.empty())
@@ -317,7 +364,7 @@ int main()
         detail::windows::removeClosed();
         detail::windows::pollEvents();
 
-        detail::commands::executeCommands();
+        detail::commands::executeCommands(draw_state);
     }
 
     if (detail::commands::_user_thread.joinable())
