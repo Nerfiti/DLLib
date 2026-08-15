9a62e9842ecdafd9cef54b7761d5ac9607335491
Nerfit <penkov.da@phystech.edu>
2026-08-15 23:38:45 +0300

    split library into separate files

---
+++ CDLib/private/CommandManager.cpp
@@ -0,0 +1,36 @@
+#include "CommandManager.h"
+
+void CommandManager::addCommand(cmd_t&& command)
+{
+    std::unique_lock lock(commandListMutex_);
+
+    commandListReadyToWrite_.wait(lock, [this] { return !commandListFull_; });
+
+    commands_.emplace_back(std::move(command));
+}
+
+void CommandManager::sendCommands()
+{
+    commandListFull_.store(true);
+}
+
+void CommandManager::executeCommands(DrawState& drawState)
+{
+    if (commandListFull_.load() == false)
+        return;
+
+    std::vector<cmd_t> cmds;
+    {
+        std::lock_guard guard(commandListMutex_);
+        std::swap(cmds, commands_);
+    }
+
+    commandListFull_.store(false);
+    commandListReadyToWrite_.notify_all();
+    
+
+    for (auto& command : cmds)
+    {
+        command(drawState);
+    }
+}
---
+++ CDLib/private/WindowManager.cpp
@@ -0,0 +1,68 @@
+#include "WindowManager.h"
+
+#include <SFML/Graphics/Sprite.hpp>
+
+WindowContext::WindowContext(uint32_t width, uint32_t height, std::string_view title, sf::State state)
+    : window(sf::VideoMode({width, height}), std::string(title), state)
+    , texture({width, height})
+    {}
+
+std::weak_ptr<WindowContext> WindowManager::createWindow(uint32_t width, uint32_t height, std::string_view title, sf::State state)
+{
+    auto ctx = std::make_shared<WindowContext>(width, height, title, state);
+    return windowContexts_.emplace_front(std::move(ctx));
+}
+
+std::weak_ptr<WindowContext> WindowManager::getLastWindow()
+{
+    return windowContexts_.front();
+}
+
+void WindowManager::removeClosed()
+{
+    auto it = windowContexts_.begin();
+
+    while (it != windowContexts_.end())
+    {
+        if (!(*it)->window.isOpen())
+            it = windowContexts_.erase(it);
+        else
+            ++it;
+    }
+}
+
+void WindowManager::pollEvents()
+{
+    for (auto& windowCtx : windowContexts_)
+    {
+        while (const std::optional event = windowCtx->window.pollEvent())
+            if (event->is<sf::Event::Closed>())
+                windowCtx->window.close();
+    }
+}
+
+void WindowManager::displayWindow(std::weak_ptr<WindowContext> windowHandler)
+{
+    if (windowHandler.expired())
+        return;
+    
+    auto windowCtx = windowHandler.lock();
+
+    windowCtx->texture.display();
+    sf::Sprite textureSprite(windowCtx->texture.getTexture());
+
+    windowCtx->window.clear(sf::Color::Magenta);
+    windowCtx->window.draw(textureSprite);
+    windowCtx->window.display();
+}
+
+void WindowManager::displayWindows()
+{
+    for (auto& windowCtx : windowContexts_)
+        displayWindow(windowCtx);
+}
+
+bool WindowManager::hasWindows()
+{
+    return !windowContexts_.empty();
+}
